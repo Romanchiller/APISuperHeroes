f3ce674b025f236ce187d25058db4363afe66a3e
Roman Tarabrin <VacuuM2007@yandex.ru>
2023-01-30 02:12:15 +0300

    clear

--- main.py
+++ main.py
@@ -7,8 +7,7 @@ dict_heroes = {}
 
 def get_int_hero():
     URL = "https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json"
-    params = {'name': 'Hulk'}
-    response = requests.get(url=URL, params=params)
+    response = requests.get(url=URL)
     for hero in response.json():
         if hero['name'] in list_heroes:
             dict_heroes[hero['name']] = hero['powerstats']['intelligence']
